d1118ea4dd56be33288fea366ade8f14f147f1a3
Matthew Nicol <matthew.b.nicol@gmail.com>
2019-03-27 02:25:14 +0000

    Make black|white binary for better tests. Clean up some functions

--- src/chesseng.h
+++ src/chesseng.h
@@ -13,16 +13,17 @@ int can_opponent_attack(Board*, int);
 Board get_board(Turn t);
 void apply_move (Board *b, Move *m);
 void reverse_move(Board *b, Move *m);
-int trim_invalid_positions(Board*, Move**, int);
+int trim_invalid_moves(Board*, Move**, int);
 void standard (Board *b);
 Board* copy_board(Board*);
 void applyMove(Board*, Move*); 
 PieceMovement *blankPieceMovement(void);
 PieceMovement *makePieceMovement(int, int, Piece, Piece);
 Move *makeMove(PieceMovement*, PieceMovement*);
-int same_team(Board*, int, int);
+int same_team(Piece[64], int, int);
 
 #include "board.c"
 #include "devel.c"
 #include "pieces/pieces.h"
 #include "move.c"
+#include "position.c"
--- src/definitions.c
+++ src/definitions.c
@@ -1,4 +1,4 @@
-typedef enum { PLAYER_NONE, PLAYER_WHITE, PLAYER_BLACK } Player, Turn;
+typedef enum { PLAYER_WHITE, PLAYER_BLACK } Player, Turn;
 
 typedef enum { 
     NO_PIECE,
@@ -27,26 +27,6 @@ typedef struct {
     Move *moves[];
 } Board;
 
-#define RANK1(x)   (x < 64 && x > 56)
-#define RANK2(x)   (x < 56 && x > 48)
-#define RANK3(x)   (x < 48 && x > 40)
-#define RANK4(x)   (x < 40 && x > 32)
-#define RANK5(x)   (x < 32 && x > 24)
-#define RANK6(x)   (x < 24 && x > 16)
-#define RANK7(x)   (x < 16 && x > 8)
-#define RANK8(x)   (x < 8 && x > 0)
-#define RANK(x)    (x / 8 + 1)
-#define NEXTRANK(x)        (x + 8)
-#define PREVRANK(x)        (x - 8)
-#define OCCUPIED(A, B)     (A->piecemap[B] != NO_PIECE)
-#define ONSQ(A, B, C)      (A->piecemap[B] == C)   
-#define SAMERANK(A, B)    (RANK(A) == RANK(B))
-#define PAWN(X)           (X == WHITE_PAWN || X == BLACK_PAWN || X == WHITE_EP_PAWN || X == BLACK_EP_PAWN)
-#define KNIGHT(X)           (X == WHITE_KNIGHT || X == BLACK_KNIGHT)
-#define BISHOP(X)           (X == WHITE_BISHOP || X == BLACK_BISHOP)
-#define ROOK(X)           (X == WHITE_ROOK || X == BLACK_ROOK)
-#define QUEEN(X)           (X == WHITE_QUEEN || X == BLACK_QUEEN)
-#define KING(X)           (X == WHITE_KING || X == BLACK_KING)
 #define VALID(X)         (X >= 0 && X < 64)
 
 static char *FILE_MAP[] = {
@@ -111,7 +91,6 @@ static int is_black[] =  {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 
 static int is_white[] =  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
 /* Does a board have the same side's piece in sq1 and sq2 */
-int same_team(Board *b, int sq1, int sq2) {
-    return (is_black[b->piecemap[sq1]] && is_black[b->piecemap[sq2]]) ||
-        (is_white[b->piecemap[sq1]] && is_white[b->piecemap[sq2]]);
+int same_team(Piece p[64], int sq1, int sq2) {
+    return (is_black[p[sq1]] && is_black[p[sq2]]) || (is_white[p[sq1]] && is_white[p[sq2]]);
 }
