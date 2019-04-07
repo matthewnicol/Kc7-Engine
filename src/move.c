f174dcb583a6d50b98ea0f78dcb050ae1012b36f
Matthew Nicol <matthew.b.nicol@gmail.com>
2019-04-07 14:51:54 +0000

    Establish moves for rest of pieces on chess board

--- src/move.c
+++ src/move.c
@@ -6,10 +6,13 @@ static int pawn_captures(Piece[], int, Turn, Move*);
 static int pawn_ep_captures(Piece[], int, Turn, Move*);
 static int pawn_promotions(Piece[], int, Turn, Move*);
 static int knight_moves(Piece[], int, Move*);
+static int king_moves(Piece[], int, Move*);
+static int king_castles(Piece[], int, Move*);
+static int linewise_piece_moves(Piece[], int, int, int, Move*);
 /*@null@*/ static MoveSet* make_moveset(int);
 static void kill_moveset(MoveSet*);
-static void primary_move(Move*, int, int, Piece, Piece);
-static void side_effect_move(Move*, MoveSideEffect);
+static void basic_move(Move*, int, int, Piece, Piece);
+static void move_with_side_effect(Move*, int, int, Piece, Piece, MoveSideEffect);
 
 void all_legal_moves(Piece sq[], Turn t)
 {
@@ -17,6 +20,7 @@ void all_legal_moves(Piece sq[], Turn t)
     MoveSet *m = make_moveset(30);
     assert(m != NULL);
     for (i = 0; i < 64; i++) {
+        if ((t && is_white[sq[i]]) || (!t && is_black[sq[i]])) continue;
         if (is_pawn[sq[i]]) {
             m->count += pawn_advances(sq, i, t, m->moves+m->count); 
             m->count += pawn_captures(sq, i, t, m->moves+m->count);
@@ -24,6 +28,15 @@ void all_legal_moves(Piece sq[], Turn t)
             m->count += pawn_promotions(sq, i, t, m->moves+m->count);
         } else if (is_knight[sq[i]]) {
             m->count += knight_moves(sq, i, m->moves+m->count);
+        } else if (is_king[sq[i]]) {
+            m->count += king_moves(sq, i, m->moves+m->count);
+            m->count += king_castles(sq, i, m->moves+m->count);
+        } else if (is_bishop[sq[i]]) {
+            m->count += linewise_piece_moves(sq, i, 1, 0, m->moves+m->count);
+        } else if (is_rook[sq[i]]) {
+            m->count += linewise_piece_moves(sq, i, 0, 1, m->moves+m->count);
+        } else if (is_queen[sq[i]]) {
+            m->count += linewise_piece_moves(sq, i, 1, 1, m->moves+m->count);
         }
     }
 
@@ -53,11 +66,11 @@ static int pawn_advances(Piece sq[], int square, Turn t, Move *m)
     );
 
     if (can_move_1) {
-        primary_move((m + i++), square, square + (8*direction), sq[square], NO_PIECE);
+        basic_move((m + i++), square, square + (8*direction), sq[square], NO_PIECE);
     }
 
     if (can_move_2) {
-        primary_move((m + i++), square, square + (16*direction), sq[square], NO_PIECE);
+        basic_move((m + i++), square, square + (16*direction), sq[square], NO_PIECE);
     } 
     return i;
 }
@@ -66,10 +79,10 @@ static int pawn_captures(Piece sq[], int square, Turn t, Move *m)
 {
     int i = 0, direction = t? 1 : -1;
     if (different_team(sq, square, square+(9*direction))) {
-        primary_move((m + i++), square, square + (9*direction), sq[square], sq[square+(9*direction)]);
+        basic_move((m + i++), square, square + (9*direction), sq[square], sq[square+(9*direction)]);
     }
     if (different_team(sq, square, square+(7*direction))) {
-        primary_move((m + i++), square, square + (7*direction), sq[square], sq[square+(7*direction)]);
+        basic_move((m + i++), square, square + (7*direction), sq[square], sq[square+(7*direction)]);
     }
     return i;
 }
@@ -82,12 +95,22 @@ static int pawn_ep_captures(Piece sq[], int square, Turn t, Move *m)
     char ep2_file = t? 'h' : 'a';
 
     if (sq[square+(-1*direction)] == them && FILE_MAP[square+(-1*direction)] != ep1_file) {
-        primary_move((m + i++), square, square + (9*direction), sq[square], sq[square+(9*direction)]);
-        side_effect_move((m + i-1), EP_CAPTURE);
+        move_with_side_effect(
+            (m + i++), 
+            square, 
+            square + (9*direction), 
+            sq[square], 
+            sq[square+(9*direction)],
+            EP_CAPTURE);
     }
     if (sq[square+(1*direction)] == them && FILE_MAP[square+(1*direction)] != ep2_file) {
-        primary_move((m + i++), square, square + (7*direction), sq[square], sq[square+(7*direction)]);
-        side_effect_move((m + i-1), EP_CAPTURE);
+        move_with_side_effect(
+            (m + i++), 
+            square, 
+            square + (7*direction), 
+            sq[square], 
+            sq[square+(7*direction)],
+            EP_CAPTURE);
     }
     return i;
 }
@@ -106,8 +129,13 @@ static int pawn_promotions(Piece sq[], int square, Turn t, Move *m)
     if (on_promote_rank && !sq[square+(8*direction)])
     {
         for (j = 0; j < 4; j++) {
-            primary_move((m + i++), square, square + (8*direction), sq[square], promotions[t][j]);
-            side_effect_move((m + i-1), PROMOTION);
+            move_with_side_effect(
+                (m + i++), 
+                square, 
+                square + (8*direction), 
+                sq[square], 
+                promotions[t][j],
+                PROMOTION);
         }
     }
     return i;
@@ -117,32 +145,99 @@ static int pawn_promotions(Piece sq[], int square, Turn t, Move *m)
 static int knight_moves(Piece sq[], int square, Move *m)
 {
     static int knight_diffs[][2] = {
-        {-1, 2}, {1, 2}, {-1, -2}, {1, -2},
-        {2, -1}, {2, 1}, {-2, -1}, {-2, 1},
+        {-1, 2}, {1, 2}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}, {-2, -1}, {-2, 1},
     };
 
     static char bad_knight_jumps [][2] = {
-        {'a', 'h'}, {'a', 'g'}, {'b', 'h'},
-        {'h', 'a'}, {'g', 'a'}, {'h', 'b'}
+        {'a', 'h'}, {'a', 'g'}, {'b', 'h'}, {'h', 'a'}, {'g', 'a'}, {'h', 'b'}
     };
 
     int i = 0, j;
 
     for (j = 0; j < 8; j++) {
-        int sqto = square + knight_diffs[j][0]*8 + knight_diffs[j][1];
+        int sqto = square + (knight_diffs[j][0]*8) + (knight_diffs[j][1]);
         if (VALID(sqto) && (!sq[sqto] || different_team(sq, square, sqto))) {
             int k, clipping = 0;
             for (k = 0; k < 6; k++) {
-                if (FILE_MAP[sqto] == bad_knight_jumps[j][0] && FILE_MAP[square] == bad_knight_jumps[j][1]) {
+                if (FILE_MAP[sqto] == bad_knight_jumps[k][0] && FILE_MAP[square] == bad_knight_jumps[k][1]) {
                     clipping = 1;
                     break;
                 }
             }
-            if (!clipping) primary_move((m+i++), square, sqto, sq[square], sq[sqto]); 
+            if (!clipping) basic_move((m+i++), square, sqto, sq[square], sq[sqto]); 
         }
     } 
     return i;
+}
 
+static int king_moves(Piece sq[], int square, Move *m)
+{
+    static int king_diffs[][2] = {
+        {0, 1}, {0, -1}, {1, 1}, {1, -1}, {1, 0}, {-1, 1}, {-1, -1}, {-1, 0}
+    };
+
+    int i = 0, j;
+
+    for (j = 0; j < 8; j++) {
+        int sqto = square + king_diffs[j][0]*8 + king_diffs[j][1];
+        if (VALID(sqto) && (!sq[sqto] || different_team(sq, square, sqto))) {
+            basic_move((m+i++), square, sqto, sq[square], sq[sqto]);
+        }
+    }
+    return i;
+}
+
+static int linewise_piece_moves(Piece sq[], int square, int diagonal, int upanddown, Move *m)
+{
+    int i = 0, j, k;
+    int blockaded[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    for (k = 1; k < 8; k++) {
+        int directions[8] = {
+            square + k,
+            square - k,
+            square - (k * 8),
+            square + (k * 8),
+            square + (k * 8) + k,
+            square - (k * 8) - k,
+            square - (k * 8) + k,
+            square + (k * 8) - k,
+        };
+        for (j = 0; j < 8; j++) {
+            if (!VALID(directions[j])) continue;
+            if (!diagonal && j > 3) continue;
+            if (!upanddown && j < 4) continue;
+            if (blockaded[j]) continue;
+
+            blockaded[j] = sq[directions[j]] > 0;
+            if ((blockaded[j] && different_team(sq, square, directions[j])) || !blockaded[j]) {
+                basic_move((m+i++), square, directions[j], sq[square], sq[directions[j]]);
+            }
+        }
+    }
+    return i;
+}
+
+
+static int king_castles(Piece sq[], int square, Move *m)
+{
+    static Piece castling_king[] = {BLACK_CASTLING_KING, WHITE_CASTLING_KING};
+    static Piece castling_rook[] = {BLACK_CASTLING_ROOK, WHITE_CASTLING_ROOK};
+    static Piece moved_king[] = {BLACK_KING, WHITE_KING};
+    int piece_index = is_white[sq[square]];
+    int i = 0;
+    int ks = is_black[sq[square]] ? 4 : 60;
+
+    if (!(square == ks && sq[square] != castling_king[piece_index]))
+        return 0;
+
+    if (sq[square+3] == castling_rook[piece_index] && !sq[square+1] && !sq[square+2]) {
+        move_with_side_effect((m+i++), square, square+2, moved_king[piece_index], NO_PIECE, CASTLE);
+    }
+    if (sq[square-4] == castling_rook[piece_index] && !sq[square-1] && !sq[square-2] && !sq[square-3]) {
+        move_with_side_effect((m+i++), square, square+2, moved_king[piece_index], NO_PIECE, CASTLE);
+    }
+
+    return i;
 }
 
 static MoveSet *make_moveset(int size)
@@ -181,15 +276,20 @@ static void kill_moveset(MoveSet* m)
     }
 }
 
-static void primary_move(Move *m, int from, int to, Piece on_from, Piece on_to)
+static void basic_move(Move *m, int from, int to, Piece on_from, Piece on_to)
 {
+    assert(from >= 0 && from < 64);
+    assert(to >= 0 && to < 64);
+    assert((int)on_from >= 0 && on_from < 22);
+    assert((int)on_to >= 0 && on_to < 22);
     m->from = from;
     m->to = to;
     m->on_from = on_from;
     m->on_to = on_to;
 }
 
-static void side_effect_move(Move *m, MoveSideEffect mse)
+static void move_with_side_effect(Move *m, int from, int to, Piece on_from, Piece on_to, MoveSideEffect mse)
 {
+    basic_move(m, from, to, on_from, on_to);
     m->side_effect = mse;
 }
