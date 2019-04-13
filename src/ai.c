715f322459d2839c555b4a0a08ae34757b76df6e
Matthew Nicol <matthew.b.nicol@gmail.com>
2019-04-13 12:24:23 +0930

    Naive but at least working version of minmax

--- src/ai.c
+++ src/ai.c
@@ -9,12 +9,15 @@ void make_random_move(Board *b, MoveSet *m)
     }
 }
 
-float evaluate(Board *b, MoveSet *m) 
+double evaluate(Board *b, MoveSet *m) 
 {
     if (is_checkmate(b->squares, m)) {
-        return b->turn ? -999 : 999;
+        return b->turn ? -999.0 : 999.0;
+    } else if (is_stalemate(b->squares, m)) {
+            return 0.0;
     }
-    int i, piece_scores = 0;
+    int i; 
+    float piece_scores = 0;
     int bishops[] = {0, 0};
     // Close game
     for (i = 0; i < 64; i++) {
@@ -24,33 +27,48 @@ float evaluate(Board *b, MoveSet *m)
     return piece_scores + (bishops[0] == 2 ? .5 : 0.0) + (bishops[1] == 2 ? -.5 : 0.0);
 }
 
-float minmax(Board *b, int depth)
+double minmax(Board *b, int depth, Move *outmove)
 {
     MoveSet *m = all_legal_moves(b->squares, b->turn);
-    float result = 1000.0, tmp = 0.0;
-
+    
     if (depth == 0) {
-        result = evaluate(b, m);
+        double result = evaluate(b, m);
+        free(m->moves);
         free(m);
         return result;
     } else {
+        double tmp, best;
         b->turn = !b->turn;
+        apply_move(b->squares, &m->moves[0]);
+        printf("A NEW CONTENDER");
+        printMove(9999, &m->moves[0]);
+        best = minmax(b, depth-1, outmove);
+        (*outmove).from = m->moves[0].from;
+        (*outmove).to = m->moves[0].to;
+        (*outmove).on_from = m->moves[0].on_from;
+        (*outmove).on_to = m->moves[0].on_to;
+        (*outmove).side_effect = m->moves[0].side_effect;
+        reverse_move(b->squares, &m->moves[0]);
         int i;
-        for (i = 0; i < m->count; i++) {
+        for (i = 1; i < m->count; i++) {
             apply_move(b->squares, &m->moves[i]);
-            if (result == 1000) {
-                result = minmax(b, depth-1);
-            } else {
-                tmp = minmax(b, depth-1);
-                if ((b->turn && tmp < result) || (b->turn && tmp > result)) {
-                    result = tmp;
-                }
+            tmp = minmax(b, depth-1, outmove);
+            if ((b->turn && tmp < best) || (!b->turn && tmp > best)) {
+                best = tmp;
+                printf("A NEW CONTENDER");
+                printMove(9999, &m->moves[i]);
+                (*outmove).from = m->moves[i].from;
+                (*outmove).to = m->moves[i].to;
+                (*outmove).on_from = m->moves[i].on_from;
+                (*outmove).on_to = m->moves[i].on_to;
+                (*outmove).side_effect = m->moves[0].side_effect;
             }
             reverse_move(b->squares, &m->moves[i]);
         }
+        free(m->moves); 
+        free(m);
+        b->turn = !b->turn;
+        return best;
     }
-        
-    free(m);
-    b->turn = !b->turn;
-    return result;
 }
+
